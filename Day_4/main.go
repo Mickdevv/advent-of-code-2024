9a2d8c2a1f0d3e5cfd201aa0749d08e93211d704
Mickdevv <michael@devereux.fm>
2024-12-04 12:58:44 +0100

    First attempt, part 1 unfinished

--- Day_4/main.go
+++ Day_4/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 )
 
+var notFound = [4][2]int{{0,0},{0,0},{0,0},{0,0}}
+
 func main(){
 	file, err := os.Open("Day_4/input_test.txt")
 	if err != nil {
@@ -14,9 +16,76 @@ func main(){
 	}
 
 	scanner := bufio.NewScanner(file)
-
+	var table []string
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		table = append(table, line)
 	}
+
+	P1(table)
+}
+
+func P1(table []string) {
+	total := 0
+	for lineIndex, lineValue := range(table) {
+		for charIndex, charValue := range(lineValue) {
+			if string(charValue) == "X" {
+				total += searchAround(table, charIndex, lineIndex)
+			}
+		}
+	}
+	fmt.Println(total)
+}
+
+func searchAround(table []string, x int, y int) int {
+	// fmt.Println(table, x, y)
+	total := 0
+
+	//check inline to the right
+	if x + len("XMAS") < len(table[y]) && table[y][x:x+len("XMAS")] == "XMAS" {
+		total ++
+		fmt.Println(table[y], "XMAS")
+	}
+	//check inline to the left
+	if x - len("XMAS") >= 0 && table[y][x - len("XMAS")+1:x+1] == "SAMX" {
+		total ++
+		fmt.Println(table[y], "SAMX")
+	}
+
+	XMAS := "XMAS"
+
+	upRight := 0
+	upLeft := 0
+	downRight := 0
+	downLeft := 0
+
+	for i := 0; i < len(XMAS); i++ {
+		if i < len(table) && i < len(table[y]) && table[y][x] == XMAS[i] {
+			upRight ++
+		}
+		if i < len(table) && i < len(table[y]) && table[y][x] == XMAS[i] {
+			upLeft ++
+		}
+		if i < len(table) && i < len(table[y]) && table[y][x] == XMAS[i] {
+			downRight ++
+		}
+		if i < len(table) && i < len(table[y]) && table[y][x] == XMAS[i] {
+			downLeft ++
+		}
+	}
+
+	if upRight == 4 {
+		total++
+	}
+	if upLeft == 4 {
+		total++
+	}
+	if downRight == 4 {
+		total++
+	}
+	if downLeft == 4 {
+		total++
+	}
+	return total
+
 }
